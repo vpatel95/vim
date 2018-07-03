51a23c89d7af36d8f61b6b36c09b76b8ef99fdbc
Ved Patel <vedmpatel@gmail.com>
2018-07-03 22:31:07 +0530

    Modified install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf ~/.vim/bundle
+cp .vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp -r scripts/* bundle/
 vim +PluginInstall +qall!
